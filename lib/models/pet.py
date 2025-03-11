e38677a13085a401f769c11b21723a39f11c7a0c
pantomimer <12694276+pantomimer@users.noreply.github.com>
2025-03-11 10:38:40 +0900

    shuruiをkindにきれいに変更

--- lib/models/pet.py
+++ lib/models/pet.py
@@ -7,10 +7,10 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.Integer, db.ForeignKey('owners.id')) #ownersというテーブルの
     name = db.Column(db.String(30), nullable=False)
-    shurui = db.Column(db.String(30), nullable=False)
+    kind = db.Column(db.String(30), nullable=False)
     owner = db.relationship('Owner', uselist=False)
 
-    def __init__(self, owner_id, name, shurui, owner):
+    def __init__(self, owner_id, name, kind, owner):
         self.owner_id = owner_id
         self.name = name
-        self.shurui = shurui
+        self.kind = kind
